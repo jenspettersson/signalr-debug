cb956577ae6cf13b72ff223eab6a868993d3db27
Jens Pettersson <jens.pettersson@live.se>
2022-06-27 15:19:01 +0200

    sample hub and client to reproduce potential bug

---
+++ SignalrHubLab.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Send message by pressing enter");
+while (true)
+{
+    var readLine = Console.ReadKey();
+
+    var hubConnection = new HubConnectionBuilder()
+        .WithUrl("http://localhost:5067/hub")
+        .Build();
+
+    await hubConnection.StartAsync();
+
+    var message = Guid.NewGuid().ToString().Substring(0, 8);
+    Console.WriteLine($"Connection-id: {hubConnection.ConnectionId}: {message} - {hubConnection.State}");
+    
+    await hubConnection.SendAsync("TestMethod", message);
+
+    //Without this, it doesn't deliver 100% of the SendAsync above.
+    //await Task.Delay(1);
+    
+    await hubConnection.StopAsync();
+
+}
---
+++ SignalrHubLab/LabHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrHubLab;
+
+public class LabHub : Hub
+{
+    private readonly ILogger<LabHub> _logger;
+
+    public LabHub(ILogger<LabHub> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        _logger.LogWarning($"Connected: {Context.ConnectionId}");
+        return Task.CompletedTask;
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        _logger.LogWarning($"Disconnected: {Context.ConnectionId}");
+        return Task.CompletedTask;
+    }
+
+    public async Task TestMethod(string message)
+    {
+        _logger.LogWarning($"========================================================== : {message}");
+        await Clients.All.SendAsync("Test", message);
+    }
+}
